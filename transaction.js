84c6d6c54717979b697e8c98e73b2433d9ca2203
JeronimoIvan <70720775+JeronimoIvan@users.noreply.github.com>
2023-06-19 14:22:41 +0200

    Update transaction.js

--- transaction.js
+++ transaction.js
@@ -1,15 +1,15 @@
 
 const Transaction = require('mpesa-mz-nodejs-lib');
-require("dotenv").config();
+//require("dotenv").config();
 
 const config = {
-    public_key: process.env.PUBLIC_KEY,
+    /*public_key: process.env.PUBLIC_KEY,
     api_host: process.env.API_HOST,
     api_key: process.env.API_KEY,
     origin: process.env.ORIGIN,
     service_provider_code: process.env.SERVICE_PROVIDER_CODE,
     initiator_identifier: process.env.INITIATOR_IDENTIFIER,
-    security_credential: process.env.SECURITY_CREDENTIAL,
+    security_credential: process.env.SECURITY_CREDENTIAL,*/
 }
 
 const transaction = new Transaction(config)
