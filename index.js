5769e073d55e3aba15a5bf2c3e49f237d17fc53c
JeronimoIvan <70720775+JeronimoIvan@users.noreply.github.com>
2023-06-19 13:08:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/check', (req, res) => {
-  res.send('working!')
+app.post('/pay', (req, res) => {
+  const amount = req.body.amount;
+  const phone = req.body.phone;
+  const reference = req.body.reference;
+  const third_party_reference = req.body.third_party_reference;
+
+  res.json({ amount, phone, reference, third_party_reference });
 })
 
 app.listen(process.env.PORT || 3000)
