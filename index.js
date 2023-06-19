3850598b42a0132c659752dab11068f9af79e015
JeronimoIvan <70720775+JeronimoIvan@users.noreply.github.com>
2023-06-19 14:35:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
   res.send('Hello World! ');
 })
 
+const config = {
+    public_key: process.env.PUBLIC_KEY,
+    api_host: process.env.API_HOST,
+    api_key: process.env.API_KEY,
+    origin: process.env.ORIGIN,
+    service_provider_code: process.env.SERVICE_PROVIDER_CODE,
+    initiator_identifier: process.env.INITIATOR_IDENTIFIER,
+    security_credential: process.env.SECURITY_CREDENTIAL,
+}
+
 app.post('/pay', (req, res) => {
   const amount = req.body.amount;
   const phone = req.body.phone;
