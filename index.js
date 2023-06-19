a6817f485c81b4ef48775807cc50a7ea6d05b312
JeronimoIvan <70720775+JeronimoIvan@users.noreply.github.com>
2023-06-19 14:57:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const Transaction = require('mpesa-mz-nodejs-lib');
 const app = express()
+
+const config = {
+    public_key: process.env.PUBLIC_KEY,
+    api_host: process.env.API_HOST,
+    api_key: process.env.API_KEY,
+    origin: process.env.ORIGIN,
+    service_provider_code: process.env.SERVICE_PROVIDER_CODE,
+    initiator_identifier: process.env.INITIATOR_IDENTIFIER,
+    security_credential: process.env.SECURITY_CREDENTIAL,
+}
+
+const transaction = new Transaction(config);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +28,11 @@ app.post('/pay', (req, res) => {
   const third_party_reference = req.body.third_party_reference;
 
   if(amount && phone && reference && third_party_reference){
-    res.json({ amount, phone, reference, third_party_reference });
+    try {
+      res.json({ amount, phone, reference });
+    } catch (error) {
+      res.json(error);
+    }
   }
    res.end();
 })
