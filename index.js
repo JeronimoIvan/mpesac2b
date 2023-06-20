da0b8e75de1b09c36112755d84c7c14f843425fe
JeronimoIvan <70720775+JeronimoIvan@users.noreply.github.com>
2023-06-20 10:13:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const express = require('express');
-const Transaction = require('mpesa-mz-nodejs-lib');
-const app = express()
+const express = require("express");
+const Transaction = require("mpesa-mz-nodejs-lib");
+const app = express();
 
 const config = {
-    public_key: process.env.PUBLIC_KEY,
-    api_host: process.env.API_HOST,
-    api_key: process.env.API_KEY,
-    origin: process.env.ORIGIN,
-    service_provider_code: process.env.SERVICE_PROVIDER_CODE,
-    initiator_identifier: process.env.INITIATOR_IDENTIFIER,
-    security_credential: process.env.SECURITY_CREDENTIAL,
-}
+  public_key: process.env.PUBLIC_KEY,
+  api_host: process.env.API_HOST,
+  api_key: process.env.API_KEY,
+  origin: process.env.ORIGIN,
+  service_provider_code: process.env.SERVICE_PROVIDER_CODE,
+  initiator_identifier: process.env.INITIATOR_IDENTIFIER,
+  security_credential: process.env.SECURITY_CREDENTIAL,
+};
 
 const transaction = new Transaction(config);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World! ');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! ");
+});
 
-app.post('/pay', async (req, res) => {
+app.post("/pay", async (req, res) => {
   const amount = req.body.amount;
   const phone = req.body.phone;
   const reference = req.body.reference;
   const third_party_reference = req.body.third_party_reference;
 
-    // res.write(process.env.API_KEY);
+  // res.write(process.env.API_KEY);
 
-  if(amount && phone && reference && third_party_reference){
-      
-    try {
+  if (amount && phone && reference && third_party_reference) {
+    const date = new Date();
+
+    const timestamp =
+      date.getFullYear() +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      ("0" + date.getDate()).slice(-2) +
+      ("0" + date.getHours()).slice(-2) +
+      ("0" + date.getMinutes()).slice(-2) +
+      ("0" + date.getSeconds()).slice(-2) +
+      ("0" + date.getMilliseconds()).slice(-2);
+
+      res.write(timestamp);
+
+    /*try {
         const response = await transaction.c2b({
           amount: amount,
           msisdn: phone,
@@ -41,11 +53,11 @@ app.post('/pay', async (req, res) => {
         res.json(response);
     } catch (error) {
         res.json(error);
-    }
-  }else{
-      res.write('Missing query parameters.');
+    }*/
+  } else {
+    res.write("Missing query parameters.");
   }
   res.end();
-})
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
